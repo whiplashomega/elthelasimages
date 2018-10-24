7eb5a0b232dd96d60e99aa4f7aa95b3c35d62f29
Joe Harrison <whiplashomega@gmail.com>
2018-10-24 14:55:16 +0000

    ready to go

---
+++ config/node.js
@@ -0,0 +1,5 @@
+/* globals MONGODB_URI, process, module */
+
+module.exports = {
+    port: process.env.PORT
+}
---
+++ server.conf.js
@@ -0,0 +1,31 @@
+// modules =================================================
+var express        = require('express');
+var app            = express();
+// configuration ===========================================
+
+// config files
+var config = require('./config/node');
+// set our port
+var path = require('path');
+global.appRoot = path.resolve(__dirname) + config.staticDir;
+
+app.start = function() { 
+    console.log("starting");
+    
+    app.use(express.static(__dirname + '/images', { maxAge: 365 }));
+    
+    // set the static files location /public/img will be /img for users
+    app.use(express.static('.')); 
+    console.log("express static loaded");
+    // start app ===============================================
+    // startup our app at http://localhost:8080
+    app.listen(config.port);
+    // shoutout to the user                     
+    console.log('Magic happens on port ' + config.port);    
+}
+
+app.stop = function() {
+    app.close();
+}
+// expose app           
+exports = module.exports = app;    
